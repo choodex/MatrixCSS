673428a834935252a68d7c355f53e3e14723d411
Chris Hood <chood9275@gmail.com>
2016-07-31 23:23:41 -0400

    Moved some settings to the top of JS

--- src/matrix.js
+++ src/matrix.js
@@ -1,13 +1,19 @@
-createDiv();
-createDiv();
-createDiv();
+//Settings
+var MAX_Z_INDEX = 5;
+var NUM_DIVS = 20;
+var DOC_WIDTH = $(window).width();  //Limit to size of viewport for now
+
+
+for(var x = 0; x < NUM_DIVS; x++) {
+    createDiv();
+}
 
 function createDiv() {
    var aDiv = document.createElement("div");
    var divID = 'aDiv' + getRandomIntInclusive(0,10000);
-   var yPos = getRandomIntInclusive(0, $(document).width());
+   var yPos = getRandomIntInclusive(0, DOC_WIDTH);
    aDiv.setAttribute("id", divID);
-   aDiv.setAttribute("style", "z-index:" + getRandomIntInclusive(1,3) + "; top:0px; left:"+ yPos + "px;")
+   aDiv.setAttribute("style", "z-index:" + getRandomIntInclusive(0,MAX_Z_INDEX) + "; left:"+ yPos + "px;")
    aDiv.innerHTML = "<span>&#25935;</span><br/><span>&#25463;</span><br/><span>&#30340;</span><br/><span>&#26837;</span><br/><span>&#33394;</span><br/><span>&#29392;</span><br/><span>&#29432;</span>";
    $("body").prepend(aDiv);
    addAnimation(divID);

8823adba7986318f80b6a603d799608bee4dc61f
Chris Hood <chood9275@gmail.com>
2016-07-31 21:49:50 -0400

    Animation now loops endlessly and cleans up after itself

--- src/matrix.js
+++ src/matrix.js
@@ -1,10 +1,13 @@
 // $("#a").addClass('animFall');
 // $("#e").addClass('animFlair');
 
-createDiv('a');
+createDiv();
+createDiv();
+createDiv();
 
-function createDiv(divID) {
+function createDiv() {
    var aDiv = document.createElement("div");
+   var divID = 'aDiv' + getRandomIntInclusive(0,10000);
    aDiv.setAttribute("id", divID);
    aDiv.innerHTML = "<span>&#25935;</span><br/><span>&#25463;</span><br/><span>&#30340;</span><br/><span>&#26837;</span><br/><span>&#33394;</span><br/><span>&#29392;</span><br/><span>&#29432;</span>";
    $("body").prepend(aDiv);
@@ -12,24 +15,31 @@ function createDiv(divID) {
 }
 
 function addAnimation(divID) {
-   //Get all direct children
+   //Get all direct children (i.e. the spans)
    var column = $("div#" + divID + " > span");
    var lastIndex = column.length - 1;
 
    column.each(function(index) {
        $(this).css({
-        "animation-name": "flair",
-        "animation-duration": "2.2s",
-        "animation-delay": (index/2) + "s",
-        "animation-iteration-count": "1",
-        "animation-timing-function": "ease-out"
+           "animation-name": "flair",
+           "animation-duration": "2.2s",
+           "animation-delay": (index/2) + "s",
+           "animation-iteration-count": "1",
+           "animation-timing-function": "ease-out"
        });
        if(index == lastIndex) {
+           //Just once, create a new Div to replace this one, and remove this one.
            $(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
-           function(e) {
-               //repeat once
-               createDiv('d');
-           });
+              function(e) {
+                  createDiv();
+                  $("div#" + e.currentTarget.parentNode.id).remove();
+              });
        }
    });
 } //end myAnimate();
+
+
+// Returns a random integer between min (included) and max (included)
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}

5eb35c7352f348e44197a072654b2b15f8adef01
Chris Hood <chood9275@gmail.com>
2016-08-09 23:58:27 -0400

    Some performance refactoring. Consolidated some looping. (Only saved about 2% CPU time)

--- src/matrix.js
+++ src/matrix.js
@@ -1,6 +1,6 @@
 //Settings
 var MATRIX_DIV_CLASS = 'div.matrixScreen';
-var MAX_Z_INDEX = 2;
+var MAX_Z_INDEX = 3;
 var MAX_WIDTH;
 var MAX_HEIGHT;
 var CONTAINER_OFFSET_Y;
@@ -19,60 +19,54 @@ function refreshSettings() {
     NUM_SPANS = Math.floor(MAX_HEIGHT / 20);
 
     for(var x = 0; x < NUM_DIVS; x++) {
-        createDiv();
+        createStream();
     }
 }
 
-function createDiv() {
+//Each div represents a single 'stream' of characters
+function createStream() {
     var aDiv = document.createElement("div");
     var aRandom = getRandomIntInclusive(0, 500);
 
-    var divID = 'stream' + aRandom;
-    aDiv.setAttribute("id", divID);
+    var streamID = 'stream' + aRandom;
+    aDiv.setAttribute("id", streamID);
 
     aDiv.setAttribute("class", "stream");
-    aDiv.setAttribute("style", "z-index:" + (aRandom % MAX_Z_INDEX) + "; left:" + (getRandomIntInclusive(0, MAX_WIDTH) + CONTAINER_OFFSET_Y) + "px;");
+    aDiv.setAttribute("style", "z-index: " + (aRandom % MAX_Z_INDEX));
 
-    var text = '';
-    for(var x = 0; x < NUM_SPANS; x++) {
-        text += "<span>" + getCharacterEncoding() + "</span><br/>"
-    }
-    aDiv.innerHTML = text;
+    $(MATRIX_DIV_CLASS).prepend(aDiv);    
 
-    $(MATRIX_DIV_CLASS).prepend(aDiv);
-    addAnimationToDiv(divID);
+    refreshStreamContent(streamID);
 }
 
-function addAnimationToDiv(divID) {
-    //Get all direct children of the div/stream (i.e. all of the spans)
-    var stream = $("div#" + divID + " > span");
-    var lastSpanIndex = stream.length - 1;
-    var animSpeed = getRandomIntInclusive(900, 2900);
+//Add a new set of spans to the given Stream. Includes adding the animation as well.
+function refreshStreamContent(streamID) {
+    var aDiv = $('#' + streamID);
+    var animSpeed = getRandomIntInclusive(900, 2900);    
+    var text = '';
 
-    //Add the animation to each span
-    stream.each(function (index) {
-        $(this).css({
-            "animation-name": "flare",
-            "animation-duration": "2200ms",
-            "animation-delay": (animSpeed * index / lastSpanIndex) + "ms",
-            "animation-iteration-count": "1",
-            "animation-timing-function": "ease-out"
+    for(var x = 0; x < NUM_SPANS; x++) {
+        text += "<span style='animation-name:flare;" +
+                             "animation-duration:2200ms;" +
+                             "animation-delay:"+ (animSpeed * x / NUM_SPANS) + "ms;" +
+                             "animation-iteration-count:1;" +
+                             "animation-timing-function: ease-out;" +
+                             "'>" + getCharacterEncoding() + "</span><br/>";
+    }
+    $(aDiv).html(text);
+
+    //Update to new random position
+    $(aDiv).css("left", (getRandomIntInclusive(0, MAX_WIDTH) + CONTAINER_OFFSET_Y) + "px");
+    
+    $("#" + streamID + " > span:last").one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+        function (e) {
+            refreshStreamContent(streamID);
         });
-        if(index == lastSpanIndex) {
-            //Just once, after the last span animates
-            $(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
-                function (e) {
-                    //Make a new div and remove this one from DOM
-                    createDiv();
-                    $("div#" + e.currentTarget.parentNode.id).remove();
-                });
-        }
-    });
-} //end myAnimate();
+}
 
+//Char encodings for half-width kana characters
+//Could also include some Latin chars in range 383-450
 function getCharacterEncoding() {
-    //Char encodings for half-width kana characters
-    //Could also include characters in range 383-450, for Latin chars
     return "&#" + getRandomIntInclusive(65381, 65437) + ";";
 }
 
